4de3d057f4b5ad714ff6db3353b34b6666f60133
Alexandr71 <tzhaphqiel92@gmail.com>
2025-06-08 03:43:19 +0300

    1

--- Ввод данных в словарь.py
+++ Ввод данных в словарь.py
@@ -3,11 +3,14 @@ data = {'height': '180',
         'age': '33' 
         }
 user_h = input('Введи свой рост ')
-print('Your height =', user_h,','' My height =', data['height'])
+print('Your height =', user_h,','
+      ' My height =', data['height'])
 
 user_w = input('Введи свой вес ')
-print('Your weight =', user_w,','' My weight =', data['weight'])
+print('Your weight =', user_w,','
+      ' My weight =', data['weight'])
 
 user_a = input('Введи свой возраст ')
-print('Your age =', user_a,','' My age =', data['age'])
+print('Your age =', user_a,','
+      ' My age =', data['age'])
 
