e016982a097f237812e214b0c42f83860086a810
Alexandr71 <tzhaphqiel92@gmail.com>
2025-06-08 03:42:07 +0300

    1

---
+++ Ввод данных в словарь.py
@@ -0,0 +1,13 @@
+data = {'height': '180',
+        'weight': '84',
+        'age': '33' 
+        }
+user_h = input('Введи свой рост ')
+print('Your height =', user_h,','' My height =', data['height'])
+
+user_w = input('Введи свой вес ')
+print('Your weight =', user_w,','' My weight =', data['weight'])
+
+user_a = input('Введи свой возраст ')
+print('Your age =', user_a,','' My age =', data['age'])
+
