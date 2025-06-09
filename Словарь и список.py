16cdc124b6243c5e5edf0b0e49730a16f6c48c99
Alexandr71 <tzhaphqiel92@gmail.com>
2025-06-09 16:43:57 +0300

    1

---
+++ Словарь и список.py
@@ -0,0 +1,9 @@
+aria_pesni = ['torero', 'angelskaya pyl', 'probil chas']
+ruki_vverh_pesni = ['devchonki', 'on tebya celuet', 'chuzhie guby']
+ac_dc_pesni = ['thunder stuck', 'hells bells', 'TNT']
+dict_muz = {
+    'Aria' : [aria_pesni],
+    'ruki_vverh' : [ruki_vverh_pesni],
+    'ac_dc' : [ac_dc_pesni]
+}
+print(dict_muz)
