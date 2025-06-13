4a9fd7d7a9c5f2ed4528809a0a2a04d2c25be6b1
Alexandr71 <tzhaphqiel92@gmail.com>
2025-06-13 11:59:09 +0300

    1

---
+++ Строки/Принимает от пользователя две строки.py
@@ -0,0 +1,4 @@
+first_otvet = input(str("Что вы делали вчера? "))
+second_otvet = input(str("Что планируете делать сегодня? "))
+text = "Вчера я {}, сегодня я планирую {} ".format(first_otvet, second_otvet)
+print(text)
